4220c41cc94958950eddc22f383cd1d86fc13e46
= <=>
2026-06-25 22:34:23 -0400

    Refactor module structure

--- Modules/WinEventTools/tests/_bootstrap.ps1
+++ Modules/WinEventTools/tests/_bootstrap.ps1
@@ -3,4 +3,4 @@ while ($root -and -not (Test-Path (Join-Path $root "src"))) {
     $root = Split-Path $root -Parent
 }
 
-Import-Module (Join-Path $root "src\WinEventTools") -Force
+Import-Module (Join-Path $root "WinEventTools.psm1") -Force
